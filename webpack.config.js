6d4c5926ff5ebb1178403b5eea6ebf4126ce8333
Josh Mullins <mullinsjoshuaj@gmail.com>
2017-08-10 00:10:16 -0400

    Updated webpack to compile pug file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 var babelOptions = {
   "presets": [
@@ -17,9 +18,10 @@ module.exports = {
   cache: true,
   entry: {
     main: './src/module.ts',
-    vendor: [
-      'babel-polyfill',
-    ]
+    // other: './src/other.js',
+    // vendor: [
+    //   'babel-polyfill',
+    // ]
   },
   output: {
     path: path.resolve(__dirname, './dist'),
@@ -39,27 +41,40 @@ module.exports = {
           loader: 'ts-loader'
         }
       ]
-    }, {
-      test: /\.js$/,
-      exclude: /node_modules/,
+    },
+    // {
+    //   test: /\.js$/,
+    //   exclude: /node_modules/,
+    //   use: [
+    //     {
+    //       loader: 'babel-loader',
+    //       options: babelOptions
+    //     }
+    //   ]
+    // }, 
+    {
+      test: /\.pug$/,
       use: [
         {
-          loader: 'babel-loader',
-          options: babelOptions
+          loader: 'pug-loader'
         }
       ]
-    }]
+    },
+    ]
   },
   plugins: [
     new CopyWebpackPlugin([
-            // {output}/file.txt
-            { from: './manifest.json' },
-            { from: './images/selenium-ide-logo-16.png', to: './assets/images/selenium-ide-logo-16.png' },
-            { from: './images/selenium-ide-logo-24.png', to: './assets/images/selenium-ide-logo-24.png' },
-            { from: './images/selenium-ide-logo-48.png', to: './assets/images/selenium-ide-logo-48.png' },
-            { from: './images/selenium-ide-logo-128.png', to: './assets/images/selenium-ide-logo-128.png' },
-            { from: './images/selenium-ide-logo-512.png', to: './assets/images/selenium-ide-logo-512.png' },
-    ])
+      { from: './manifest.json' },
+      { from: './images/selenium-ide-logo-16.png', to: './assets/images/selenium-ide-logo-16.png' },
+      { from: './images/selenium-ide-logo-24.png', to: './assets/images/selenium-ide-logo-24.png' },
+      { from: './images/selenium-ide-logo-48.png', to: './assets/images/selenium-ide-logo-48.png' },
+      { from: './images/selenium-ide-logo-128.png', to: './assets/images/selenium-ide-logo-128.png' },
+      { from: './images/selenium-ide-logo-512.png', to: './assets/images/selenium-ide-logo-512.png' },
+    ]),
+    new HtmlWebpackPlugin({
+      filename: './options.html',
+      template: './src/views/options.pug'
+    }),
   ],
   resolve: {
     extensions: ['.ts', '.js']
